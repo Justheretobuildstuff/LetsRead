898804382ee052a950779238653b392e59d4c448
Josh <josh.daniels10@gmail.com>
2018-09-12 23:01:02 -0700

    updates to server file

--- server.js
+++ server.js
@@ -9,5 +9,51 @@ var cheerio = require('cheerio');
 var fs = require('fs');
 var axios = require('axios');
 
-//Requiring models
+// Requiring models setting up my port and initializing express
+var db = require("./models");
+var PORT = 8080;
+var app = express();
+
+// Configuring middleware
+
+// Morgan logger for logging requests
+app.use(logger("dev"));
+// Using Body-Parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Using express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// Connect to Mongo DB
+mongoose.connect("mongodb://localhost/8080");
+
+// Routes
+
+// Creating GET route for scraping the echoJS site
+app.get("/scrape", function(req, res) {
+  axios.get("https://www.bloomberg.com/").then(function(response) {
+    var $ = cheerio.load(response.data);
+
+    $("main h3").each(function(i, element) {
+      var result = {};
+      // Adding text and link to result object
+      result.title = $(this)
+      .children("a")
+      .text();
+      result.link = $(this)
+      .children("a")
+      .attr("href");
+      // Creating new Article with result data
+      db.Article.create(result)
+      .then(function(dbArticle) {
+        console.log(dbArticle);
+      })
+      .catch(function(err) {
+        return res.json(err);
+      });
+    });
+    // Returning indication that the scrape was successful to the client
+    res.send("Hey, that scrape worked!")
+  });
+});
+
 
